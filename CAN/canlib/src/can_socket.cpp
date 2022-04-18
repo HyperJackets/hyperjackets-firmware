48b242bccf0a7fe3b47cacda48ec74c27fcfad0c
Anthony Hong <ahong75@gatech.edu>
2022-04-18 16:53:47 -0400

    Added message struct and send/receive functions

--- CAN/canlib/src/can_socket.cpp
+++ CAN/canlib/src/can_socket.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <cstdint.h>
 
 #include <net/if.h>
 #include <sys/ioctl.h>
@@ -10,30 +11,65 @@
 
 #include <linux/can.h>
 #include <linux/can/raw.h>
+typedef uint32_t u32;
 
-static int s;
+static int s; // socket
 static struct sockaddr_can addr;
 static struct ifreq ifr;
+static string bus = "vcan0"; // Change to whatever BUS we want to use
 
 void CANSocket::connectCANBus() {
+    // Sock raw prevents TCP/IP encapsulation packet processing
+    if ((s = socket(AF_INET, SOCK_RAW, 0)) < 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+    // Change to can1 later for actual use
+    strcpy(ifr.ifr_name, bus); 
+    // Retrieving index of chosen CAN interface
+    ioctl(s, SIOCGIFINDEX, &ifr);
 
-  // Sock raw prevents TCP/IP encapsulation packet processing
-  if ((s = socket(AF_INET, SOCK_RAW, 0)) < 0) {
-    perror("socket failed");
-    exit(EXIT_FAILURE);
-  }
-  // Change to can1 later for actual use
-  strcpy(ifr.ifr_name, "vcan0"); 
-  // Retrieving index of chosen CAN interface
-  ioctl(s, SIOCGIFINDEX, &ifr);
+    // Binding the socket to CAN interface
+    memset(&addr, 0, sizeof(addr));
+    addr.can_family = AF_CAN;
+    addr.can_ifindex = ifr.ifr_ifindex;
 
-  // Binding the socket to CAN interface
-  memset(&addr, 0, sizeof(addr));
-  addr.can_family = AF_CAN;
-  addr.can_ifindex = ifr.ifr_ifindex;
+    if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+}
 
-  if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-    perror("Bind failed");
-    exit(EXIT_FAILURE);
-  }
+void CANSocket::sendMessage(u32 id, u32 data) {
+    struct can_frame frame;
+
+    frame.can_id = id;
+    frame.can_dlc = 8;
+    for (int i = 0; i < 8; i++) {
+        frame.data[i] = (data >> 4*i) & 255;
+    }
+    if (write(s, &frame, sizeof(struct can_frame)) != sizeof(struct can_frame)) {
+        perror("Message send failed");
+        exit(EXIT_FAILURE);
+    }
+}
+
+Message CANSocket::receiveMessage() {
+    Message msg;
+    int num_bytes;
+    struct can_frame frame;
+
+    num_bytes = read(s, &frame, sizeof(struct can_frame));
+
+    if (num_bytes < 0) {
+        perror("Message read failed");
+        exit(EXIT_FAILURE);
+    }
+    msg.id = frame.can_id;
+    int data = 0;
+    for (int i = 0; i < 8; i++) {
+        data |= (frame.data[i] << 4*i);
+    }
+    msg.data = data;
+    return msg;
 }
--- CAN/canlib/src/include/can_socket.hpp
+++ CAN/canlib/src/include/can_socket.hpp
@@ -1,6 +1,32 @@
+#ifndef can_socket_h
+#define can_socket_h
+#include <cstdint.h>
+typedef uint32_t u32;
+
+//The structure of a raw CAN frame
+//struct can_frame {
+//    canid_t can_id;  /* 32 bit CAN_ID + EFF/RTR/ERR flags */
+//    __u8    can_dlc; /* frame payload length in byte (0 .. 8) */
+//    __u8    __pad;   /* padding */
+//    __u8    __res0;  /* reserved / padding */
+//    __u8    __res1;  /* reserved / padding */
+//    __u8    data[8] __attribute__((aligned(8)));
+//};
+
+// Message used as a wrapper for can messages. An unsigned int is used for the data because it makes the bitwise shifts easier when trying to parse the raw CAN payload, which is an 8 byte character array.
+struct Message {
+    bool received; // this is for when reading the bus returns nothing
+    unsigned int id; // this is also unsigned for consistency
+    unsigned int data; // for right now, we aren't going to worry about floats
+}
+
 class CANSocket {
 public:
-  int k_PORT = 8000;
+    int k_PORT = 8000;
 
-  void connectCANBus();
+    void connectCANBus();
+    void sendMessage();
+    Message receiveMessage();
 };
+
+#endif
